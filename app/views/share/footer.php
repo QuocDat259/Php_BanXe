13bd6941a530f531b7a7f1c0e06560778c8893f3
QuocDat259 <cunktvn06@gmail.com>
2024-04-14 17:03:12 +0700

    fisrt commit

---
+++ app/views/share/footer.php
@@ -0,0 +1,54 @@
+</div>
+
+<!-- Sidebar -->
+<div class="col-md-3">
+    <div class="banner">
+        <img src="app/image/banner.png" style="width: 100%; height: auto;" />
+    </div>
+
+</div>
+
+</div>
+</div>
+<!-- Phần footer -->
+<div class=" mt-4 " style="background-color:#fa5230">
+    <div class="row">
+        <div class="col-md-4">
+            <div class=" text-white text-center py-4">
+                <h3>CHĂM SÓC KHÁCH HÀNG</h3>
+                <p>Trung Tâm Trợ Giúp</p>
+                <p>Shop Blog</p>
+                <p>Chính Sách Bảo Hành</p>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class=" text-center text-white p-4">
+                <h5>THEO DÕI CHÚNG TÔI</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class=" text-white text-center py-4">
+                <h3>TẢI ỨNG DỤNG NGAY THÔI</h3>
+                <img src="app/image/footer.png" />
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center py-4">
+            <!-- Thẻ div để chứa bản đồ -->
+            <div id="map" style="height: 300px;">
+                <!-- Nhúng iframe của Google Maps vào đây -->
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8066.428170695558!2d106.77774751432587!3d10.85742961783838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175276e7ea103df%3A0xb6cf10bb7d719327!2zSFVURUNIIC0gxJDhuqFpIGjhu41jIEPDtG5nIG5naOG7hyBUUC5IQ00gKFRodSBEdWMgQ2FtcHVzKQ!5e0!3m2!1svi!2s!4v" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
